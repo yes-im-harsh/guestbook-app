06a796176d6d452cd22b579052d3be30a805edd6
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-11-01 23:17:37 +0530

    NextAuth Implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,35 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
 
-const Home: NextPage = (props) => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+const Home: NextPage = () => {
+  const { data: session, status } = useSession();
 
-  return <main>Guestbook App</main>;
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
+  return (
+    <main>
+      <h1>Guestbook App</h1>
+      <div>
+        {session ? (
+          <>
+            <p>hi {session.user?.name}</p>
+            <button onClick={() => signOut()}>Logout</button>
+          </>
+        ) : (
+          <button
+            onClick={() => signIn("discord")}
+            className="rounded-lg bg-blue-500 p-2"
+          >
+            Login with Discord
+          </button>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-  const { data: sessionData } = useSession();
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.name}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <Link
-        className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </Link>
-    </section>
-  );
-};
