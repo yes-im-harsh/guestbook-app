5fb841eda97b1177439a8dea64b1b9b0849abe02
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-11-02 12:06:13 +0530

    Created Mutation and Protected it using the protectedProcedure

---
+++ src/server/trpc/router/guestbook.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { protectedProcedure, router } from "../trpc";
+
+export const guestbook = router({
+  postMessage: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
