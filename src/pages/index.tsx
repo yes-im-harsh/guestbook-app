54430ee401b8dfbcd7ea4a02135f686c0b0145d6
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-11-03 23:32:27 +0530

    All Features Implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,7 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import { useState } from "react";
 import { trpc } from "../utils/trpc";
-
-const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") {
-    return <main className="flex flex-col items-center pt-4">Loading...</main>;
-  }
-
-  return (
-    <main className="flex flex-col items-center">
-      <h1 className="pt-4 text-3xl">Guestbook App</h1>
-
-      <div className="pt-10">
-        <div>
-          {session ? (
-            <>
-              <p>hi {session.user?.name}</p>
-              <button onClick={() => signOut()}>Logout</button>
-            </>
-          ) : (
-            <button
-              onClick={() => signIn("discord")}
-              className="rounded-lg bg-blue-500 p-2"
-            >
-              Login with Discord
-            </button>
-          )}
-          <div className="pt-10">
-            <Messages />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
-
-export default Home;
 
 const Messages = () => {
   const { data: message, isLoading } = trpc.guestbook.getAll.useQuery();
@@ -58,3 +21,99 @@ const Messages = () => {
     </div>
   );
 };
+
+const Form = () => {
+  const [message, setMessage] = useState("");
+  const { data: session } = useSession();
+  const utils = trpc.useContext();
+
+  const postMessage = trpc.guestbook.postMessage.useMutation({
+    onMutate: () => {
+      utils.guestbook.getAll.cancel();
+      const optimisticUpdate = utils.guestbook.getAll.getData();
+
+      if (optimisticUpdate) {
+        utils.guestbook.getAll.setData(optimisticUpdate);
+      }
+    },
+
+    onSettled: () => {
+      utils.guestbook.getAll.invalidate();
+    },
+  });
+
+  return (
+    <form
+      className="flex gap-2"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (session !== null) {
+          postMessage.mutate({
+            name: session.user?.name as string,
+            message,
+          });
+        }
+
+        setMessage("");
+      }}
+    >
+      <input
+        type="text"
+        value={message}
+        placeholder="Your message..."
+        minLength={2}
+        maxLength={100}
+        onChange={(event) => setMessage(event.target.value)}
+        className="rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+      />
+      <button
+        type="submit"
+        className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+const Home: NextPage = (props) => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main className="flex flex-col items-center pt-4">Loading...</main>;
+  }
+
+  return (
+    <main className="flex flex-col items-center">
+      <h1 className="pt-4 text-3xl">Guestbook App</h1>
+
+      <div className="pt-10">
+        <div>
+          {session ? (
+            <>
+              <p>hi {session.user?.name}</p>
+              <button onClick={() => signOut()}>Logout</button>
+
+              <div className="pt-6">
+                <Form />
+              </div>
+            </>
+          ) : (
+            <button
+              onClick={() => signIn("discord")}
+              className="rounded-lg bg-blue-500 p-2"
+            >
+              Login with Discord
+            </button>
+          )}
+          <div className="pt-10">
+            <Messages />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- src/server/trpc/router/guestbook.ts
+++ src/server/trpc/router/guestbook.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 import { protectedProcedure, router, publicProcedure } from "../trpc";
 
-export const guestbook = router({
+export const guestbookRouter = router({
   postMessage: protectedProcedure
     .input(
       z.object({
         name: z.string(),
         message: z.string(),
       })
-    ).mutation(async ({ ctx, input }) => {
-    
+    )
+    .mutation(async ({ ctx, input }) => {
       try {
         await ctx.prisma.guestbook.create({
           data: {
@@ -21,9 +21,7 @@ export const guestbook = router({
         console.log(error);
       }
     }),
-});
 
-export const guestbookRouter = router({
   getAll: publicProcedure.query(async ({ ctx }) => {
     try {
       return await ctx.prisma.guestbook.findMany({
